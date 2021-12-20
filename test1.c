25f7ec0306592519e1c2c5ec41796ec71fa2e95f
m9xxx <91776712+m9xxx@users.noreply.github.com>
2021-12-20 11:46:25 +0300

    Added library

--- test1.c
+++ test1.c
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <ctype.h>
 #include <time.h>
+#include <malloc.h>
 
 const char *months[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 int err_count = 0;
